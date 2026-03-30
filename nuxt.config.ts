fcb158c62dfbf5cfd59a5160400cae9009859656
prazim-projects <managervonxlique@gmail.com>
2026-03-30 20:28:14 +0300

    css conflict fix attempt 2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,7 +10,7 @@ export default defineNuxtConfig({
   },
   css: [
     'bootstrap/dist/css/bootstrap.min.css',
-    '~/public/css/main.css',
+    '~/assets/css/tailwind.css',
   ],
   modules: ['@nuxtjs/tailwindcss'],
 
