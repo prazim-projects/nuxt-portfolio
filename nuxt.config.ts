48d2a82f12a69d22c23d5ea2b50b3a89d9a37231
prazim-projects <managervonxlique@gmail.com>
2026-03-30 20:13:38 +0300

    css order

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,17 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: false },
-  modules: ['@nuxtjs/tailwindcss'],
+  ssr: false,
+  app: {
+    baseURL: '/'
+  },
+  nitro: {
+    preset: 'github-pages'
+  },
   css: [
     'bootstrap/dist/css/bootstrap.min.css',
     '~/assets/css/main.css',
   ],
-  ssr: false,
-  nitro: {
-    preset: 'github-pages'
-  },
-  app: {
-    baseURL: '/'
-  }
+  modules: ['@nuxtjs/tailwindcss'],
+
 })
