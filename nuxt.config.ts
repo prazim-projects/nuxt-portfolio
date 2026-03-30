4cdc435c4064748f350423e156e07b2312cf0e47
prazim-projects <managervonxlique@gmail.com>
2026-03-30 18:39:24 +0300

    github pages customization

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   modules: ['@nuxtjs/tailwindcss'],
   css: [
     'bootstrap/dist/css/bootstrap.min.css',
     '~/assets/css/main.css',
-  ]
+  ],
+  ssr: false,
+  nitro: {
+    preset: 'github-pages'
+  }
 })
