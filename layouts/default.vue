603b8427ef8f714e595dc28ff0e783deda77998b
Bealprasim Demere <managervonxlique@gmail.com>
2026-04-22 10:14:06 -0400

    added google analytics functionality.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,32 @@
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-61WJGN61YF"></script>
+<template>
+  <Nuxt />
+</template>
+
 <script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'G-61WJGN61YF');
+export default {
+  head() {
+    return {
+      script: [
+        {
+          src: "https://www.googletagmanager.com/gtag/js?id=G-61WJGN61YF",
+          async: true
+        },
+        {
+          hid: "gtag-init",
+          innerHTML: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-61WJGN61YF');
+          `,
+          type: "text/javascript",
+          charset: "utf-8"
+        }
+      ],
+      __dangerouslyDisableSanitizersByTagID: {
+        'gtag-init': ['innerHTML']
+      }
+    };
+  }
+}
 </script>
